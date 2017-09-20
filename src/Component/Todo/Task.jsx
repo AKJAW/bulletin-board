e392eae1ba4d2a45ae76a78fc39ec84469d84fd9
Olek <akjaworski1@gmail.com>
2017-09-20 13:00:54 +0200

    new build with console.log

--- src/Component/Todo/Task.jsx
+++ src/Component/Todo/Task.jsx
@@ -9,6 +9,7 @@ class Task extends React.Component {
 	render() {
 		// debugger;
 		// debugger;
+		console.log(this.props.labelObject['color'])
 		return (
 			<Droppable droppableId={`droppable-${this.props.labelName}`}>
 				{(provided, snapshot) => (
@@ -17,60 +18,37 @@ class Task extends React.Component {
 						padding: 8,
 						width: 100
 					}}>
-						{this.props.labelObject['tasks'].map((item, liIterator) => (
-							<Draggable key={`item-${this.props.labelName}-${liIterator}`} draggableId={`item-${this.props.labelName}-${liIterator}`}>
-								{(provided, snapshot) => (
-									<div>
-										<div ref={provided.innerRef} style={
-											[provided.draggableStyle,
-											{
-												// some basic styles to make the items look a bit nicer
-												userSelect: 'none',
-												padding: 8 * 2,
-											margin: `0 0 ${8}px 0`,
+						{this.props.labelObject['tasks'].map(function(item, liIterator) {
+							const key = Object.keys(item)[0];
+							return(
+								<Draggable key={`item-${this.props.labelName}-${liIterator}`} draggableId={`item-${this.props.labelName}-${liIterator}`}>
+									{(provided, snapshot) => (
+										<div>
+											<div ref={provided.innerRef} style={
+												[provided.draggableStyle,
+												{
+													// some basic styles to make the items look a bit nicer
+													userSelect: 'none',
+													padding: 8 * 2,
+													margin: `0 0 ${8}px 0`,
 
-												// change background colour if dragging
-												background: snapshot.isDragging ? 'lightgreen' : 'grey',
-											}]}
-											{...provided.dragHandleProps}>
-											{Object.keys(item)[0]}
+													// change background colour if dragging
+													background: snapshot.isDragging ? 'lightgreen' : 'grey',
+												}]}
+												{...provided.dragHandleProps}>
+												{key}
+												{item[key]}
+											</div>
+											{provided.placeholder}
 										</div>
-										{provided.placeholder}
-									</div>
-								)}
-							</Draggable>
-						))}
+									)}
+								</Draggable>
+							)
+						}.bind(this))}
 						{provided.placeholder}
 					</div>
 				)}
 			</Droppable>
-
-			// <div style={styles.dropZone} innerRef={dropProvided.innerRef}>
-			// 	{this.props.labelObject['tasks'].map((item, liIterator) => (
-			// 		<Draggable key={`item-${this.props.labelName}-${liIterator}`} draggableId={`item-${this.props.labelName}-${liIterator}`}>
-			// 			{(providedChild, snapshotChild) => (
-			// 				<div>
-			// 					<div ref={providedChild.innerRef} style={
-			// 						[providedChild.draggableStyle,
-			// 						{
-			// 							// some basic styles to make the items look a bit nicer
-			// 							userSelect: 'none',
-			// 							padding: 8 * 2,
-			// 							margin: `0 0 ${8}px 0`,
-			//
-			// 							// change background colour if dragging
-			// 							backgroundColor: snapshotChild.isDragging ? 'lightgreen' : 'red',
-			// 						}]}
-			// 						{...providedChild.dragHandleProps}>
-			// 						{Object.keys(item)[0]}
-			// 					</div>
-			// 					{providedChild.placeholder}
-			// 				</div>
-			// 			)}
-			// 		</Draggable>
-			// 	))}
-			// 	{providedChild.placeholder}
-			// </div>
 		)
 }
 }
